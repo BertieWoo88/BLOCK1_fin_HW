08aff28e24921fd52e752bb373d481ddb65bfa24
BertieWoo88 <050688V@gmail.com>
2023-03-22 09:43:43 +0300

    GetArr and PrintArr methods added

--- Program.cs
+++ Program.cs
@@ -3,3 +3,30 @@
 выполнения алгоритма. При решении не рекоменщется пользоваться коллеюкциями, лучше обойтись 
 исключительно массивами. */
 
+using System;
+using static System.Console;
+
+Clear();
+string[] check = GetArr();
+Printarr(check);
+
+string [] GetArr() // метод получения массива строк из одной введеной строки
+{
+    WriteLine("Введите cтроки для массива строк через пробел: ");
+    string Strforarray = ReadLine()!;
+    string[] result = Strforarray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    return result;
+}
+
+
+void Printarr (string [] arr) // Метод ввыода массива строк
+{
+  Console.Write("[ ");
+  for(int i = 0; i < arr.Length-1; i ++)
+  {
+    Console.Write($"{arr[i]}, ");
+  }
+    Console.Write($"{arr[arr.Length-1]} ");
+    Console.Write("]");
+}
+
